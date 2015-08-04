fbb94ea36fcddb52ba773bfa13b3a9a4f09bb306
Aviv Levinski <pugsiman@gmail.com>
2015-08-04 18:59:18 +0300

    second test

--- movie_ratings/core exercises.sql
+++ movie_ratings/core exercises.sql
@@ -28,8 +28,17 @@ where Reviewer.rID = Rating.rID and ratingDate is null;
 select name, title, stars, ratingDate
 from Movie, Rating, Reviewer
 where Movie.mID = Rating.mID and Reviewer.rID = Rating.rID
+order by name, title, stars;
 
 --Q6
+-- For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie.
+select name, title
+from Movie, Reviewer, (select R1.rID, R1.mID from Rating R1, Rating R2 where
+R1.rID = R2.rID
+ and R1.mID = R2.mID
+ and R1.stars < R2.stars
+ and R1.ratingDate < R2.ratingDate) N
+where Movie.mID = N.mID and Reviewer.rID = N.rID;
 
 --Q7
 
